00a23b72f560182ffe384013818fffc6f779a9f0
Simon Kornblith <simon@simonster.com>
2015-04-29 19:41:50 -0400

    Indentation -> spaces

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,44 +2,44 @@ using PiecewiseIncreasingRanges
 using Base.Test
 
 function test(rgs, divisor...)
-	vcrgs = vcat(rgs...)
-	!isempty(divisor) && (vcrgs /= divisor[1])
-	rg = PiecewiseIncreasingRange(rgs, divisor...)
-	@test length(rg.ranges) == 4
-	@test vcrgs == rg
+    vcrgs = vcat(rgs...)
+    !isempty(divisor) && (vcrgs /= divisor[1])
+    rg = PiecewiseIncreasingRange(rgs, divisor...)
+    @test length(rg.ranges) == 4
+    @test vcrgs == rg
 
-	for i = 1:length(rg)
-		@test searchsortedfirst(rg, rg[i]) == i
-		@test searchsortedfirst(rg, rg[i]-1//17) == i
-		@test searchsortedfirst(rg, rg[i]+1//17) == i+1
+    for i = 1:length(rg)
+        @test searchsortedfirst(rg, rg[i]) == i
+        @test searchsortedfirst(rg, rg[i]-1//17) == i
+        @test searchsortedfirst(rg, rg[i]+1//17) == i+1
 
-		@test searchsortedlast(rg, rg[i]) == i
-		@test searchsortedlast(rg, rg[i]-1//17) == i-1
-		@test searchsortedlast(rg, rg[i]+1//17) == i
+        @test searchsortedlast(rg, rg[i]) == i
+        @test searchsortedlast(rg, rg[i]-1//17) == i-1
+        @test searchsortedlast(rg, rg[i]+1//17) == i
 
-		for within_half_sample in (false, true)
-			@test findnearest(rg, rg[i], within_half_sample) == i
-			@test findnearest(rg, rg[i]+1//16, within_half_sample) == i
-			@test findnearest(rg, rg[i]-1//17, within_half_sample) == i
-			@test findnearest(rg, rg[i]+1//17, within_half_sample) == i
-		end
+        for within_half_sample in (false, true)
+            @test findnearest(rg, rg[i], within_half_sample) == i
+            @test findnearest(rg, rg[i]+1//16, within_half_sample) == i
+            @test findnearest(rg, rg[i]-1//17, within_half_sample) == i
+            @test findnearest(rg, rg[i]+1//17, within_half_sample) == i
+        end
 
-		for j = i:length(rg)
-			@test vcrgs[i:j] == rg[i:j]
-			for k = 1:i-j+1
-				@test vcrgs[i:k:j] == rg[i:k:j]
-			end
-		end
-	end
+        for j = i:length(rg)
+            @test vcrgs[i:j] == rg[i:j]
+            for k = 1:i-j+1
+                @test vcrgs[i:k:j] == rg[i:k:j]
+            end
+        end
+    end
 
-	@test findnearest(rg, -1) == 1
-	@test_throws NoNearestSampleError findnearest(rg, -1, true)
-	@test findnearest(rg, 15.9) == searchsortedfirst(rg, 15)
-	@test findnearest(rg, 16.1) == searchsortedfirst(rg, 17)
-	@test_throws NoNearestSampleError findnearest(rg, 15.9, true)
-	@test_throws NoNearestSampleError findnearest(rg, 16.1, true)
-	@test findnearest(rg, 19) == length(rg)
-	@test_throws NoNearestSampleError findnearest(rg, 19, true)
+    @test findnearest(rg, -1) == 1
+    @test_throws NoNearestSampleError findnearest(rg, -1, true)
+    @test findnearest(rg, 15.9) == searchsortedfirst(rg, 15)
+    @test findnearest(rg, 16.1) == searchsortedfirst(rg, 17)
+    @test_throws NoNearestSampleError findnearest(rg, 15.9, true)
+    @test_throws NoNearestSampleError findnearest(rg, 16.1, true)
+    @test findnearest(rg, 19) == length(rg)
+    @test_throws NoNearestSampleError findnearest(rg, 19, true)
 end
 
 test(StepRange{Rational{Int},Rational{Int}}[0:1//2:5, 5+1//8:1//8:10, 10+1//4:1//4:14, 14+1//4:1//4:15, 17:1//2:18])
